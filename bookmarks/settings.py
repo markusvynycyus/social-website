e9c23c86d97e08fc18bad656e1856959d95af49c
marku <markusvynycyus@hotmail.com>
2021-04-01 19:52:04 -0300

    Teste com a Base de Dados

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',# configuracao de deploy, envia arquivos staticos
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
