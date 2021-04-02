bd772e6b4447c9b0bb733b81245ea8a1948b6e49
marku <markusvynycyus@hotmail.com>
2021-04-02 08:57:10 -0300

    Criando App e Model para marcacçao e amarzanamento de Imagens.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'imagens.apps.ImagensConfig',
+    'imagens.apps.ImagensConfig', # app imagens
 
 ]
 
