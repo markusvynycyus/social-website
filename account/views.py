2d13d2930d905a01ff21cd483375cd9e875b655c
marku <markusvynycyus@hotmail.com>
2021-04-02 08:18:01 -0300

    Backend de Autenticacao Customizada - permite por email.

--- account/views.py
+++ account/views.py
@@ -73,9 +73,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated successfully')
+            messages.success(request, 'Perfil atualizado com sucesso.')
         else:
-            messages.error(request, 'Error updating your profile')
+            messages.error(request, 'Error ao atualizar o perfil.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -149,3 +149,11 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    #'social_core.backends.facebook.FacebookOAuth2',
+    #'social_core.backends.twitter.TwitterOAuth',
+    #'social_core.backends.google.GoogleOAuth2',
+]
